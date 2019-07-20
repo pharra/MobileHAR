8900a8f57a62fba17b80840600aa7d91dd9f7fb7
pharra <typechasing@gmail.com>
2019-07-20 10:23:47 +0800

    add Classifier but no related dependencies

--- app/src/main/java/com/example/mobilehar/MainActivity.java
+++ app/src/main/java/com/example/mobilehar/MainActivity.java
@@ -8,14 +8,19 @@ import android.os.Bundle;
 import com.example.mobilehar.log.Logger;
 import com.example.mobilehar.services.GravitySensorService;
 import com.example.mobilehar.services.GyroscopeSensorService;
+import com.example.mobilehar.har.Classifier;
 
 
 public class MainActivity extends AppCompatActivity {
+    private static final String MODEL_FILE = "file:///android_asset/frozen_model.pb";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Classifier classifier = new Classifier(getAssets(), MODEL_FILE);
+        float[] test = new float[2032 * 1 * 9];
+        classifier.predict(test, 1, 2032, 9);
     }
 
     @Override
@@ -29,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onPause() {
         super.onPause();
-        //stopService(new Intent(this, GyroscopeSensorService.class));
-        //stopService(new Intent(this, GravitySensorService.class));
+        stopService(new Intent(this, GyroscopeSensorService.class));
+        stopService(new Intent(this, GravitySensorService.class));
     }
 }
---
+++ app/src/main/java/com/example/mobilehar/har/Classifier.java
@@ -0,0 +1,62 @@
+package com.example.mobilehar.har;
+
+import android.content.res.AssetManager;
+
+import com.example.mobilehar.log.Logger;
+
+import org.tensorflow.contrib.android.TensorFlowInferenceInterface;
+
+import java.nio.FloatBuffer;
+
+public class Classifier {
+    private TensorFlowInferenceInterface inferenceInterface;
+
+    // input variable name
+    private static final String x = "inputs";
+
+    // input variable name
+    private static final String y = "labels";
+
+    // predict variable name
+    private static final String pred = "logits";
+
+    private static final String[] labels = {"WALKING",
+            "WALKING_UPSTAIRS",
+            "WALKING_DOWNSTAIRS",
+            "SITTING",
+            "STANDING",
+            "LAYING",
+            "STAND_TO_SIT",
+            "SIT_TO_STAND",
+            "SIT_TO_LIE",
+            "LIE_TO_SIT",
+            "STAND_TO_LIE",
+            "LIE_TO_STAND"};
+
+    private static final int label_len = 12;
+
+    static {
+        // load libtensorflow_inference.so
+        System.loadLibrary("tensorflow_inference");
+        Logger.i("tf", "Load libtensorflow_inference.so successful");
+    }
+
+    public Classifier(AssetManager assetManager, String modePath) {
+        inferenceInterface = new TensorFlowInferenceInterface(assetManager, modePath);
+        Logger.i("tf", "Load TensorFlow mode file successful");
+    }
+
+    public int predict(float[] inputs, long... dim) {
+        inferenceInterface.feed(x, inputs, dim);
+
+        int[] _y = new int[label_len];
+        inferenceInterface.feed(y, _y, 1, 12);
+        //运行模型,run的参数必须是String[]类型
+        String[] outputNames = new String[]{pred};
+        inferenceInterface.run(outputNames);
+        //获取结果
+        FloatBuffer ret = FloatBuffer.allocate(12);
+        inferenceInterface.fetch(pred, ret);
+        return 1;
+    }
+}
